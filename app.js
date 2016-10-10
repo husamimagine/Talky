8736e3c4881249cd6330c683b9bee7470422aede
husam aljafari <husam aljafari>
2016-10-10 11:55:07 +0300

    Created package.json and app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: 'e0400e45-d40b-430e-a4b7-530daa1cfcb1', appPassword: 'SXskNJ9FcpsNC03PUKjH5nY' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
